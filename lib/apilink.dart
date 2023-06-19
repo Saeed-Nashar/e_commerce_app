64e942a377157a92b8e7e86c1e1dd53ca40e5c12
Saeed-Nashar <saednashar8@gmail.com>
2023-06-19 04:04:22 +0300

    Update apilink.dart

--- lib/apilink.dart
+++ lib/apilink.dart
@@ -1,6 +1,6 @@
 class AppLink {
 
-  static const String server = "https://ecommercetest.000.pe/ecommerce";
+  static const String server = "https://.....................";
   static const String test = "$server/test.php";
 
 
